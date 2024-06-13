2e61848ef6612634870b233ea40c84bae4739bb0
josiah-d <duhaimejosiah@gmail.com>
2024-06-13 07:41:26 -0500

    add chap 2 sandbox

--- sandbox/sandbox.py
+++ sandbox/sandbox.py
@@ -1,3 +1,5 @@
+import timeit
+
 import numpy as np
 
 np.random.seed(42)
@@ -8,36 +10,44 @@ NUM_NEURONS = 3
 inputs = np.random.randint(low=-5, high=6, size=SIZE)
 
 
-class Neuron:
+class Layer:
     """
-    This class is designed to represent a single neuron in a neural network.
+    This class is designed to represent a layer of neurons in a neural network.
     """
 
-    def __init__(self, inputs):
-        """
-        The function initializes weights and bias with random values for a neural
-        network.
-
-        Args:
-          inputs: The `inputs` parameter in the `__init__` method of your code
-        snippet is used to initialize the instance variable `self.inputs`. This
-        parameter is expected to be a numpy array representing the input data for
-        your model. The `weights` variable is then initialized with random values of
-        the same shape
-        """
+    def __init__(
+        self, inputs: np.ndarray, weights: np.ndarray = None, bias: int = None
+    ):
         self.inputs = inputs
-        self.weights = np.random.rand(*inputs.shape)
-        self.bias = np.random.randint(low=-5, high=6, size=1)
+        if weights is not None:
+            self.weights = weights
+        else:
+            self.weights = np.random.rand(*inputs.shape)
+        if bias is not None:
+            self.bias = bias
+        else:
+            self.bias = np.random.randint(low=-5, high=6, size=1)
 
     def get_output(self):
         """
         The function calculates the output by taking the sum of the element-wise
         multiplication of inputs and weights, and adding the bias.
         """
-        self.output = np.sum(inputs * self.weights) + self.bias
+        self.output = np.dot(self.inputs, self.weights) + self.bias
 
 
-l0_neurons = [Neuron(inputs=inputs) for _ in range(NUM_NEURONS)]
+inputs = np.asarray([1, 2, 3, 2.5])
+weights = np.asarray(
+    [
+        [0.2, 0.8, -0.5, 1],
+        [0.5, -0.91, 0.26, -0.5],
+        [-0.26, -0.27, 0.17, 0.87],
+    ],
+)
+biases = np.asarray([2, 3, 0.5])
+
+
+l0_neurons = [Layer(inputs=inputs, weights=w, bias=b) for w, b in zip(weights, biases)]
 l0_outputs = []
 
 for i, neuron in enumerate(l0_neurons):
@@ -48,7 +58,9 @@ for i, neuron in enumerate(l0_neurons):
     l0_outputs.append(neuron.output)
     print(f"{i}: {neuron.output = }")
 
-l1_neurons = [Neuron(inputs=np.asarray(l0_outputs)) for _ in range(NUM_NEURONS)]
+print(f"{l0_outputs = }")
+
+l1_neurons = [Layer(inputs=np.asarray(l0_outputs)) for _ in range(len(l0_outputs))]
 l1_outputs = []
 
 for i, neuron in enumerate(l1_neurons):
@@ -58,3 +70,18 @@ for i, neuron in enumerate(l1_neurons):
     print(f"{i}: {neuron.bias = }")
     l1_outputs.append(neuron.output)
     print(f"{i}: {neuron.output = }")
+
+print(f"{l1_outputs = }")
+
+
+def time_neuron_output():
+    neuron_outputs = []
+    for weight, bias in zip(weights, biases):
+        neuron = Layer(inputs, weight, bias)
+        output = neuron.get_output()
+        neuron_outputs.append(output)
+    return neuron_outputs
+
+
+execution_time = timeit.timeit("time_neuron_output()", globals=globals(), number=10000)
+print(f"Execution time over 1000 runs: {execution_time} seconds")
